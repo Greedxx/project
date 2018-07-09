414587e90133da24ee51670add27e80707a0933a
Greedxx <greedxxx@163.com>
2018-07-09 14:09:26 +0800

    9

---
+++ app/Http/Requests/FormsRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'cname' => 'required|regex:/^[\x{4e00}-\x{9fa5}]+$/u'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'cname.required'=> '填写不能为空',
+            'cname.regex'=> '请填写中文'
+        ];
+    }
+}
