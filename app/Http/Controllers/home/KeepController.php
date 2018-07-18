c3915625b75654cd8d9f3a14b966f90c293791a9
shuibuxing3 <794486534@qq.com>
2018-07-18 16:47:07 +0800

    2018 7 18

--- app/Http/Controllers/home/KeepController.php
+++ app/Http/Controllers/home/KeepController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\home;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\home\Shoucang;
+
+
 
 class KeepController extends Controller
 {
     public function keep()
-    {
-
+    {	
+    	$uid = session('userinfo.id');
+    	// dd($uid);
+    	
+ 		
     	return view('home.user.keep',['title'=>'收藏列表']);
     }
+
+
 }
