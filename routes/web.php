567563b811b7033b9f02a26a28bd13baa81e6454
Greedxx <greedxxx@163.com>
2018-07-19 21:07:55 +0800

    修改一次小细节

--- routes/web.php
+++ routes/web.php
@@ -60,13 +60,10 @@
         Route::any('home/xiu','home\PassController@xiu');
         Route::any('home/gai','home\PassController@gai');
         //密码
-<<<<<<< HEAD
-        Route::any('home/success',function(){ return view('home.success');});
+       
         //订单详情
         Route::any('home/order','home\OrderController@index');    
-=======
         
->>>>>>> 286c358175578eb3b6eecf3378002817b3b81c49
     }); 
 
     //购物车
@@ -187,11 +184,8 @@
     Route::any('/jsy','home\ShopCartController@jsy');
   
 
-<<<<<<< HEAD
     //ajaxtao 默认收货地址
     Route::any('/ajaxtao/defrev','admin\AjaxtaoController@defrev');
     
-=======
     Route::any('/ordsuccess','home\ShopCartController@ordsuccess');
->>>>>>> 286c358175578eb3b6eecf3378002817b3b81c49
- 
+    
