ec790edc1cd2a61690de6afc68c715471be822db
Greedxx <greedxxx@163.com>
2018-07-21 15:53:56 +0800

    修改首页

--- resources/views/admin/goods/index.blade.php
+++ resources/views/admin/goods/index.blade.php
@@ -266,50 +266,13 @@
             <!-- AJAX e -->
 
             <!-- 暂不与其他人样式冲突 -->
-            <style>
-            .pagination {
-                clear: both;
-                color: #7d7d7d;
-                font-size: 12px;
-                overflow: hidden;
-                padding-top: 0px;
-                padding-bottom: 0px;
-                border-top: 1px #dfdfdf solid;
-                FONT-FAMILY: "Microsoft Yahei";
-                float: right;
-                list-style: none;
-                margin:0px;
-             }
-            .pagination li{
-                float: left;
-                color: #7d7d7d;
-                font-size: 12px;
-                font-weight: bold;
-                padding: 7px 12px;
-                margin-right: 8px;
-
-             }
-            .pagination li:hover{
-                cursor: pointer;
-             }
-            .pagination .active{
-                background-color: #88a9eb;
-                color: #323232;
-                border: none;
-                background-image: none;
-                box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.25);
-            }
-            .pagination .disabled{
-                    color: #666666;
-                    cursor: default;
-            }
-            </style>
+           
 
             
             <div class="dataTables_paginate paging_full_numbers" style="" id="paginate">
-                <nav aria-label="Page navigation">
+                
                 {{ $data->appends($arr)->links() }}
-                </nav>
+                
             </div>
         </div>
     </div>
